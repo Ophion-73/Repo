8cca3629eb93a8662d727dcb0124f3a1d91169cc
Jorge Salas Leybon <146293962+JorSL1120@users.noreply.github.com>
2025-05-28 01:51:35 -0600

    continuando con la interaccion de los botones

--- Assets/TodosLosDioramas/Scripts/BotonesInicio.cs
+++ Assets/TodosLosDioramas/Scripts/BotonesInicio.cs
@@ -2,22 +2,6 @@ using UnityEngine;
 
 public class BotonesInicio : MonoBehaviour
 {
-    public GameObject panelInicio;
-    public GameObject panelRandom;
-    public GameObject panelManual;
-
-    public void MenuRandom()
-    {
-        panelInicio.SetActive(false);
-        panelRandom.SetActive(true);
-    }
-
-    public void MenuManual()
-    {
-        panelInicio.SetActive(false);
-        panelManual.SetActive(true);
-    }
-
     public void Salir()
     {
         Application.Quit();
